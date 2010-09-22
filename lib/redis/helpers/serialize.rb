60b79b2cdfbae97e16c2d6e636f5368d709a4a98
Jeshua Borges <jesh@homerun.com>
2010-09-22 16:55:01 -0700

    fixed null values scenario

--- lib/redis/helpers/serialize.rb
+++ lib/redis/helpers/serialize.rb
@@ -7,12 +7,12 @@ class Redis
       end
 
       def from_redis(value)
-        return value if options[:raw]
+        return value if value.nil? or options[:raw]
         Yajl::Parser.parse(value, :symbolize_keys => options[:symbolize_keys])
       end
 
       def from_redis_list(value)
-        return value if options[:raw]
+        return value if value.empty? or options[:raw]
         value.collect{|v| from_redis(v)}
       end
     end
