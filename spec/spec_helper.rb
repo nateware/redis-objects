0e219d471406b43da69b94654b4cd83f27739de8
Nate Wiger <nate@wiger.org>
2010-11-08 11:45:01 -0800

    make sure we are testing against proper redis-rb version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'rubygems'  # poor people still on 1.8
-
-$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
+gem 'redis', '>= 2.1.1'
 require 'redis'
 
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
 require 'bacon'
 Bacon.summary_at_exit
 
@@ -11,3 +11,4 @@ $redis = Redis.new(:host => ENV['REDIS_HOST'], :port => ENV['REDIS_PORT'])
 UNIONSTORE_KEY = 'test:unionstore'
 INTERSTORE_KEY = 'test:interstore'
 DIFFSTORE_KEY  = 'test:diffstore'
+
