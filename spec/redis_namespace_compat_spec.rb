91fdfecb908106a81bd36ecf2baf00004fe0a0ae
Wayne Larsen <wayne@larsen.st>
2011-02-23 08:49:19 +0800

    Redis::Namespace is not a dependency
    If redis-namespace is not installed, just skip
    the redis-namespace compatibility test.

--- spec/redis_namespace_compat_spec.rb
+++ spec/redis_namespace_compat_spec.rb
@@ -4,15 +4,18 @@ require 'redis/objects'
 Redis::Objects.redis = $redis
 
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../../redis-namespace/lib')
-require 'redis/namespace'
+begin
+  require 'redis/namespace'
 
-describe 'Redis::Namespace compat' do
-  it "tests the compatibility of Hash and ::Hash conflicts" do
-    ns = Redis::Namespace.new("resque", :redis => $redis)
-    ns.instance_eval { rem_namespace({"resque:x" => nil}) }.should == {"x"=>nil}
-    class Foo
-      include Redis::Objects
+  describe 'Redis::Namespace compat' do
+    it "tests the compatibility of Hash and ::Hash conflicts" do
+      ns = Redis::Namespace.new("resque", :redis => $redis)
+      ns.instance_eval { rem_namespace({"resque:x" => nil}) }.should == {"x"=>nil}
+      class Foo
+        include Redis::Objects
+      end
+      ns.instance_eval { rem_namespace({"resque:x" => nil}) }.should == {"x"=>nil}
     end
-    ns.instance_eval { rem_namespace({"resque:x" => nil}) }.should == {"x"=>nil}
   end
+rescue LoadError
 end
