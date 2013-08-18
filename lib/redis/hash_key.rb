0b2fa91b93533eac74b047d84c98d306cfd26e9c
John Maxwell <john@agileleague.com>
2013-08-17 20:50:05 -0400

    Fixes problematic typo in fill

--- lib/redis/hash_key.rb
+++ lib/redis/hash_key.rb
@@ -118,7 +118,7 @@ class Redis
 
     # Set keys in bulk if they do not exist. Takes a hash of field/values {'field1' => 'val1'}. Redis: HSETNX
     def fill(pairs={})
-      raise ArgumentErorr, "Arugment to fill must be a hash of key/value pairs" unless pairs.is_a?(::Hash)
+      raise ArgumentError, "Argument to fill must be a hash of key/value pairs" unless pairs.is_a?(::Hash)
       pairs.each do |field, value|
         redis.hsetnx(key, field, to_redis(value, options[:marshal_keys][field]))
       end
